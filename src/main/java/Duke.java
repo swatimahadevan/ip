3df001ce79d352fc5d6fe2acf9e8a3b5f7ba5d9f
swatimahadevan <e0665107@u.nus.edu>
2021-08-13 19:00:32 +0800

    Add greeting

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,13 @@
 public class Duke {
+    public static void greet(){
+        //Function to simply greet the user and exit
+        String HorizontalLine = "____________________________________________________________";
+        System.out.println(HorizontalLine);
+        System.out.println("Hello, I'm Duke\n" + "What can I do for you?");
+        System.out.println(HorizontalLine);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(HorizontalLine);
+    }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +15,6 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        greet();
     }
 }
