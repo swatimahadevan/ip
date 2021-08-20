b5f4e748b52da581ff57ae9190aaf461acfe98fa
swatimahadevan <e0665107@u.nus.edu>
2021-08-20 22:42:21 +0800

    Add task list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 public class Duke {
 
     static String horizontalDividerLine = "____________________________________________________________";
+    private static String[] tasksList = new String[100];
+    private static int numberOfTasks = 0;
 
     public static void greet() {
         String logo = " ____        _        \n"
@@ -21,19 +23,32 @@ public class Duke {
         System.out.println(horizontalDividerLine);
     }
 
-    public static void echo(String userInput) {
+    public static void addTaskToList(String task){
+        tasksList[numberOfTasks++] = task;
         System.out.println(horizontalDividerLine);
-        System.out.println(userInput);
+        System.out.println("Added: " + task);
+        System.out.println(horizontalDividerLine);
+    }
+
+    public static void printTasksList(){
+        System.out.println(horizontalDividerLine);
+        for(int i = 0; i < numberOfTasks; i ++){
+            System.out.println(i+1 + ". " + tasksList[i]);
+        }
         System.out.println(horizontalDividerLine);
     }
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         greet();
-        String userInput = in.nextLine();
-        while(!userInput.equals("bye")){
-            echo(userInput);
-            userInput = in.nextLine();
+        String input = in.nextLine();
+        while(!input.equals("bye")){
+            if(input.equals("list")) {
+                printTasksList();
+            } else {
+                addTaskToList(input);
+            }
+            input = in.nextLine();
         }
         bye();
         in.close();
