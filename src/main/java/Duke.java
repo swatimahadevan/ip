1ee3849bf41c690f83fa5bf39d0751a57944ed3d
swatimahadevan <e0665107@u.nus.edu>
2021-08-20 21:59:27 +0800

    Add echo and bye

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,20 +1,41 @@
+import java.util.Scanner;
 public class Duke {
-    public static void greet(){
-        //Function to simply greet the user and exit
-        String HorizontalLine = "____________________________________________________________";
-        System.out.println(HorizontalLine);
-        System.out.println("Hello, I'm Duke\n" + "What can I do for you?");
-        System.out.println(HorizontalLine);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(HorizontalLine);
-    }
-    public static void main(String[] args) {
+
+    static String horizontalDividerLine = "____________________________________________________________";
+
+    public static void greet() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println(horizontalDividerLine);
+        System.out.println("Hello, I'm Duke\n" + "What can I do for you?");
+        System.out.println(horizontalDividerLine);
+    }
+
+    public static void bye() {
+        System.out.println(horizontalDividerLine);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(horizontalDividerLine);
+    }
+
+    public static void echo(String userInput) {
+        System.out.println(horizontalDividerLine);
+        System.out.println(userInput);
+        System.out.println(horizontalDividerLine);
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
         greet();
+        String userInput = in.nextLine();
+        while(!userInput.equals("bye")){
+            echo(userInput);
+            userInput = in.nextLine();
+        }
+        bye();
+        in.close();
     }
 }
