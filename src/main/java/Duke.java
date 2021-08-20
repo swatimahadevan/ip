357911dade476f8b710506500cb3cc787b1a37e5
swatimahadevan <e0665107@u.nus.edu>
2021-08-21 00:44:35 +0800

    Tweaked code to comply with coding standard

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,56 +1,59 @@
 import java.util.ArrayList;
 import java.util.Scanner;
-public class Duke {
 
-    static String horizontalDividerLine = "____________________________________________________________";
+public class Duke {
     private static ArrayList<Task> tasks = new ArrayList<>();
 
-    public static void greet() {
+    public static void printDividerLine() {
+        System.out.println("____________________________________________________________");
+    }
+
+    public static void printGreetMessage() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        System.out.println(horizontalDividerLine);
+        printDividerLine();
         System.out.println("Hello, I'm Duke\n" + "What can I do for you?");
-        System.out.println(horizontalDividerLine);
+        printDividerLine();
     }
 
-    public static void bye() {
-        System.out.println(horizontalDividerLine);
+    public static void printByeMessage() {
+        printDividerLine();
         System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(horizontalDividerLine);
+        printDividerLine();
     }
 
-    public static void addTaskToList(String task){
+    public static void addTaskToList(String task) {
         tasks.add(new Task(task));
-        System.out.println(horizontalDividerLine);
+        printDividerLine();
         System.out.println("added: " + task);
-        System.out.println(horizontalDividerLine);
+        printDividerLine();
     }
 
-    public static void printTasksList(){
-        System.out.println(horizontalDividerLine);
+    public static void printTasksList() {
+        printDividerLine();
         System.out.println("Here are the tasks in your list:");
         int i = 1;
         for(Task item: tasks){
             System.out.println((i++) + ". " + item.getStatusIcon() + " " + item.getDescription());
         }
-        System.out.println(horizontalDividerLine);
+        printDividerLine();
     }
 
-    public static void markTaskAsDone(int taskIndex){
+    public static void markTaskAsDone(int taskIndex) {
         tasks.get(taskIndex - 1).markAsDone();
-        System.out.println(horizontalDividerLine);
+        printDividerLine();
         System.out.println("Nice! I've marked this task as done:");
         System.out.println("[X] " + tasks.get(taskIndex - 1).getDescription());
-        System.out.println(horizontalDividerLine);
+        printDividerLine();
     }
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        greet();
+        printGreetMessage();
         String input = in.nextLine();
         while(!input.equals("bye")){
             String[] words = input.split(" ");
@@ -66,7 +69,7 @@ public class Duke {
             }
             input = in.nextLine();
         }
-        bye();
+        printByeMessage();
         in.close();
     }
 }
